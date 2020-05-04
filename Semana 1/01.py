0e7f9f3c2c6e60a004d033554d683f3834a318d7
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2020-05-04 20:41:49 -0300

    Semana 1

---
+++ Semana 1/01.py
@@ -0,0 +1,29 @@
+# Curso "Introdução à Programação Python” do IFSP – Câmpus Sertãozinho.
+
+# Semana 1 - 11 a 18 de Março de 2020
+
+# Print, comentários e tipos de dados
+
+# Funções são blocos ou pedações de códigos predefinidos que podem ser reaproveitados.
+# Possuem parâmetros ou não.
+# Parâmetros são informações passadas para a função que utilizam este parâmetro para realizar algum cálculo ou transformação.
+# Usualmente chama-se uma função pelo seu nome seguido dos parâmetros dentro de parênteses. Ex: funcao()
+
+# Print
+
+print ("Hello World!")
+print ("oi, Python")
+
+# Comentários
+
+# Isto é um comentário
+print("Aprendendo comentários") # Isto é um comentário
+
+# Tipos de dados
+
+# Strings - dados definidos entre aspas duplas ou simples, representando texto.
+# Integer - números inteiros (sem casas decimais).
+# Float - números com casas decimais.
+# Boolean - representa verdadeiro ou falso, (True ou False).
+# Lists, dictionaries, tuplas...
+
